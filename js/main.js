3f244dc6e4d976067768bd5bdcbbe87b3395827a
Danstan <danstanochieng@gmail.com>
2022-11-29 14:48:54 +0300

    uploaded files

---
+++ js/main.js
@@ -0,0 +1,88 @@
+(function($) {
+    "use strict";
+
+    if ($.fn.classyNav) {
+        $('#roamingnav').classyNav();
+    }
+    
+    // nav scroll   
+    var myoffset = $('#dtr-header-global').height();
+    $('.navbar a[href^="#"]').click(function(){  
+        event.preventDefault();  
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top - myoffset
+        }, "slow","easeInSine");
+    });
+
+    //stickyatTop
+    $(window).on("scroll", function () {
+        if ($(this).scrollTop() > 670) {
+            $(".scrollheader").addClass("is-sticky");
+            $('.scrollheader').css('position', 'fixed');
+        } else {
+            $(".scrollheader").removeClass("is-sticky");
+            $(".scrollheader").css('position', 'relative');
+        }
+    });
+
+    //Masonary js
+    var colWidth = $(".grid-item").width();
+    window.onresize = function(){
+      var colWidth = $(".grid-item").width();
+    }
+    console.log(colWidth);
+
+    var $grid = $(".grid").masonry({
+      // options
+      itemSelector: ".grid-item",
+      columnWidth: ".grid-item",
+      // percentPosition: true,
+      gutter:20,
+      fitWidth: true
+    });
+
+    $grid.imagesLoaded().progress(function() {
+      $grid.masonry("layout");
+    });
+
+    $(document).ready(function() {
+      $('.calenderslider').owlCarousel({
+        stagePadding: 50,
+        center: true,
+        items:2,
+        autoplay:false,
+        loop:true,
+        margin:20,
+        smartSpeed:800,
+        autoplayHoverPause:true,
+        nav:true,
+        dots:true,
+        navText: ['<i class="icofont-arrow-left"></i>', 
+            '<i class="icofont-arrow-right"></i>'],
+      })
+    });
+
+    $(document).ready(function() {
+      $('#bannermain').owlCarousel({
+        items: 1,
+        slideSpeed: 2000,
+        nav: false,
+        autoplay: false, 
+        dots:true,
+        loop: true,
+        responsiveRefreshRate: 200,
+        navText: ['<i class="icofont-arrow-left"></i>', 
+            '<i class="icofont-arrow-right"></i>'],
+      })
+    });
+
+    //LightBox / Fancybox
+    $('.video-popup').fancybox({
+        openEffect: 'fade',
+        closeEffect: 'fade',
+        helpers: {
+            media: {}
+        }
+    });
+
+})(window.jQuery);
